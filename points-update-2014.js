e2b1bf7d30ec23e8a3ff7f875749e81ae1ecc630
Nick <nickgieschen@gmail.com>
2014-04-04 20:09:17 -0400

    Update points-update-2014.js

--- points-update-2014.js
+++ points-update-2014.js
@@ -1,7 +1,22 @@
 callback(
     {
-        "1":20,
-        "1a":200,
-        "1b":2000
+        "Dostoevskian Idiots":44,
+        "The Outliers":0.33,
+        "The Bail Outs":18.67,
+        "Chibe's Crushers '13":17,
+        "Ken Phelps All Stars":6,
+        "Peaches":4.00,
+        "Maya's Mysteries":1,
+        "Zach Miner Threat":0,
+        "Bullfrogs":0,
+        "Hoopleheads":-0.67,
+        "Clement St. Massive":-1,
+        "Big Inning CornKings":-2,
+        "Quantum Fastball":-2,
+        "SNK Crushers":-6,
+        "Mike Kingery":-9,
+        "Mendoza Line":-9,
+        "Necessarily Possible":-15.33,
+        "The Amazin Ukrainian":0
     }
 );
